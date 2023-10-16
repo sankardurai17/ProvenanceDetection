e6d648b5e489a5778055f381e7f86244da57da1d
sankardurai17 <17tucs203@skct.edu.in>
2023-10-16 17:21:00 -0400

    inlcuding column names to the resultMap

--- src/main/java/org/cu/adta/group21/provenancedetection/utility/ProvenanceUtil.java
+++ src/main/java/org/cu/adta/group21/provenancedetection/utility/ProvenanceUtil.java
@@ -2,15 +2,16 @@ package org.cu.adta.group21.provenancedetection.utility;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ProvenanceUtil {
     public static Map<String,String> computeProvenance(ResultSet rs){
-        Map<String, String> ann_map = new HashMap<>();
+        Map<String, String> ann_map = new LinkedHashMap<>();
         try {
             int number_of_col = rs.getMetaData().getColumnCount();
             String ann;
+            populateColumnNames(rs, number_of_col, ann_map);
             while (rs.next()) {
                 StringBuilder concatKey = new StringBuilder();
                 for (int i = 3; i < number_of_col + 1; i++) {
@@ -30,5 +31,16 @@ public class ProvenanceUtil {
 
 
         return ann_map;
+    }
+
+    private static void populateColumnNames(ResultSet rs, int number_of_col, Map<String, String> ann_map) throws SQLException {
+        int k=3;
+        StringBuilder columnName=new StringBuilder();
+        while(k<=number_of_col){
+            columnName.append(rs.getMetaData().getColumnName(k)).append(" ");
+            k++;
+        }
+        String ann_columnName= rs.getMetaData().getColumnLabel(1)+"."+ rs.getMetaData().getColumnLabel(2);
+        ann_map.put(columnName.toString().trim(),ann_columnName);
     }
 }
