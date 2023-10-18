61b02accc383149471571c54e63ab4f219a8267a
sankardurai17 <17tucs203@skct.edu.in>
2023-10-17 22:15:33 -0400

    update

--- src/main/java/org/cu/adta/group21/provenancedetection/utility/QueryParser.java
+++ src/main/java/org/cu/adta/group21/provenancedetection/utility/QueryParser.java
@@ -6,16 +6,32 @@ public class QueryParser {
     public static String parseQuery(String query) {
         String table1;
         String table2;
+        query=query.toLowerCase();
+        String[] qrySplit=query.split("from",2);
+        if(qrySplit[0].contains("*")){
+            return query;
+        }
+        else{
+            /*String[] qrySplit2=qrySplit[1].split("where");
+            if(qrySplit2[0].contains(",")){
+                String[] qrySplit3=qrySplit2[0].split(",");
+            }*/
+            query=query.replace(","," ");
+            ArrayList<String> query_list=new ArrayList<>(Arrays.asList(query.split("\\s+")));
+            int index_of_join = query_list.indexOf("where");
+            table1 = query_list.get(index_of_join - 2);
+            table2 = query_list.get(index_of_join - 1);
 
-        ArrayList<String> query_list = new ArrayList<>(Arrays.asList(query.split(" ")));
-        int index_of_join = query_list.indexOf("where");
-        table1 = query_list.get(index_of_join - 2).replace(",", "");
-        table2 = query_list.get(index_of_join - 1);
+            //inserting ann column in query
+            //query_list.add(1, table1 + ".ann as " + table1 + "_ann,");
+            //query_list.add(2, table2 + ".ann as " + table2 + "_ann");
 
-        //inserting ann column in query
-        query_list.add(1, table1 + ".ann as " + table1 + "_ann,");
-        query_list.add(2, table2 + ".ann as " + table2 + "_ann,");
+            String ann1=table1 + ".ann as " + table1 + "_ann,";
+            String ann2=table2 + ".ann as " + table2 + "_ann";
+            return qrySplit[0]+","+ann1+ann2+" from "+qrySplit[1];
 
-        return String.join(" ", query_list);
+        }
+
+
     }
 }
